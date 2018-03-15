f5448ce028636a7b1bd3ba4d1a2462e09477c335
Prashant Varanasi <prashant@uber.com>
2018-03-15 18:16:31 +1100

    Add canonical import path (go.uber.org)

--- ratelimit.go
+++ ratelimit.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package ratelimit
+package ratelimit // import "go.uber.org/ratelimit"
 
 import (
 	"sync"
