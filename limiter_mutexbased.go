11b8621e4c442b14a07fdfdadb9ff83b46e0264a
Paweł <pawel@uber.com>
2020-12-29 12:47:08 -0600

    Refactor: separate inteface from the implementations (#49)
    Just a tiny cleanup - no functional changes:
    - keep the "shared/abstract" (config, interface) in ratelimit.go
    - move the atomic implementation specific bits into "limiter_atomic"
    - move "mutexbased" into "limiter_mutexbased" so that the naming
      is more consistent
    
    Since I'm here I'm also updating the licenses.
    
    The goal is to make reasoning/futher modifications easier, which
    changes touching specific implementations only touching specific
    implementation files.

--- limiter_mutexbased.go
+++ limiter_mutexbased.go
@@ -1,4 +1,24 @@
-package ratelimit
+// Copyright (c) 2016,2020 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package ratelimit // import "go.uber.org/ratelimit"
 
 import (
 	"sync"
@@ -14,8 +34,8 @@ type mutexLimiter struct {
 	clock      Clock
 }
 
-// New returns a Limiter that will limit to the given RPS.
-func newMutexBased(rate int, opts ...Option) Limiter {
+// newMutexBased returns a new atomic based limiter.
+func newMutexBased(rate int, opts ...Option) *mutexLimiter {
 	config := buildConfig(opts)
 	l := &mutexLimiter{
 		perRequest: time.Second / time.Duration(rate),
